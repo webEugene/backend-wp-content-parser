838cb0386490704db66748ec39862c68c5a2ac90
Yevhenii <eugene.bilan@gmail.com>
2025-04-22 10:28:04 +0200

    updates and improvements

---
+++ src/common/interfaces/IClassNames.ts
@@ -0,0 +1,18 @@
+export interface IClassNames {
+  home?: {
+    title?: string;
+    content?: string;
+  };
+  page?: {
+    title?: string;
+    content?: string;
+  };
+  single?: {
+    title?: string;
+    content?: string;
+  };
+  archive?: {
+    title?: string;
+    content?: string;
+  };
+}
